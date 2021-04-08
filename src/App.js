11c3d355d05b816aef3f60c4fa86d43981e29f13
Chamith <chameeefdo@gmail.com>
2021-04-08 13:02:03 +0530

    add todo logic  completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
 import TodoForm from './Componets/TodoForm';
+import TodoList from './Componets/TodoList';
 
 function App() {
+
+
   return (
     <div className="todo-app">
-      <TodoForm/>
+      <TodoList/>
     </div>
   );
 }
